ade3d0019e1cafe34c9ab4a4e49299b4cc43ff43
Yuriy Chernov <yurgon.com@gmail.com>
2014-11-10 16:41:23 +0300

    Update setting.rb
    self.where(:name => name, :locale => (locale || I18n.locale)).first_or_initialize

--- app/models/activeadmin_settings/setting.rb
+++ app/models/activeadmin_settings/setting.rb
@@ -92,7 +92,8 @@ module ActiveadminSettings
       end
 
       def self.value(name, locale)
-        find_or_create_by_name_and_locale(name, (locale || I18n.locale)).value
+        self.where(:name => name, :locale => (locale || I18n.locale)).first_or_initialize
+        #find_or_create_by_name_and_locale(name, (locale || I18n.locale)).value
       end
     end
   end
