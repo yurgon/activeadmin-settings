644401207c011b7d9b78fe936c1ab3da428a848d
Yuriy Chernov <yurgon.com@gmail.com>
2014-11-10 19:42:18 +0300

    add value

--- app/models/activeadmin_settings/setting.rb
+++ app/models/activeadmin_settings/setting.rb
@@ -92,7 +92,7 @@ module ActiveadminSettings
       end
 
       def self.value(name, locale)
-        self.where(:name => name, :locale => (locale || I18n.locale)).first_or_initialize
+        self.where(:name => name, :locale => (locale || I18n.locale)).first_or_initialize.value
         #find_or_create_by_name_and_locale(name, (locale || I18n.locale)).value
       end
     end
